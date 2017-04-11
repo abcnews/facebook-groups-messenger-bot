a86dec43ba710831f41a28427816b313d9b654ad
Simon Elvery <simon@elvery.net>
2017-04-12 09:26:22 +1000

    tabs => spaces

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
 router.get('/', function(req, res) {
-  // res.render('index', { title: 'Express' });
   res.redirect(302, '/groupshare');
 });
 
