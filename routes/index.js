c5315dfcc77c530063e22d4a7effa49d1861d242
Simon Elvery <simon@elvery.net>
2017-04-12 16:17:43 +1000

    Empty homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.redirect(302, '/groupshare');
+  res.render('index');
 });
 
 module.exports = router;
