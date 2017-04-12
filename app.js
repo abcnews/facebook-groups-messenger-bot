1120a849b3ec4131113f62484c208771e6b96470
Simon Elvery <simon@elvery.net>
2017-04-12 16:14:15 +1000

    Remove some unnecessary code

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const exphbs  = require('express-handlebars');
 
+const app = express();
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+const env = process.env.NODE_ENV || 'development';
 
-var app = express();
-
-var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
@@ -23,28 +19,26 @@ app.engine('handlebars', exphbs({
     checksum: require('./lib/checksumify')
   }
 }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
+
 // Static assets
 app.use(express.static(path.join(__dirname, 'public'),{
   maxAge: 2628000000 // one week
 }));
-app.use(cookieParser());
 
+// Routes
 app.use('/', require('./routes/index'));
 app.use('/groupshare', require('./routes/groupshare'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -53,14 +47,14 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+      title: 'error'
     });
+  });
 }
 
 // production error handler
@@ -73,7 +67,5 @@ app.use(function(err, req, res, next) {
         title: 'error'
     });
 });
-
-var content = require('./content');
 
 module.exports = app;
