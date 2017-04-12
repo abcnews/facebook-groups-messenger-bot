2ef3ad0513787612f2d998e42f0a92d155ea43d1
Simon Elvery <simon@elvery.net>
2017-04-12 16:16:31 +1000

    A big refactor for efficiency, readability and better error handling.

--- routes/groupshare.js
+++ routes/groupshare.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const content = require('../lib/content');
+const collections = [8418312]; // dynamic collection based on top stories
 
 router.get('/', function(req, res) {
-  var content = require('../content');
-  content.groupshare([8418312], req.query.search).then(function (content) {
-    res.render('groupshare', content);
+
+  let promises = collections.map(function(id) {
+    return content.collection(id);
+  });
+
+  if (req.query.search) {
+    promises.unshift(content.search(req.query.search));
+  }
+
+  Promise.all(promises).then(function(collections) {
+    res.render('groupshare', {
+      title: 'Share ABC News story',
+      search: req.query.search,
+      collections: collections
+    });
+  }, function (err) {
+    res.status(500).render('error', {
+      title: 'Error',
+      message: 'Sorry, something went wrong, please <a href="/groupshare">try your request again</a>.'
+    });
   });
 });
 
