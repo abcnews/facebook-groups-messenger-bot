474bc20d4879d40d16c811bc872e61e038fd7083
Simon Elvery <simon@elvery.net>
2017-04-17 14:09:27 +1000

    Use standard error template for fallback page

--- routes/fallback.js
+++ routes/fallback.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('fallback');
+  res.render('error', {
+    error: {
+      status: 'Please update your Messenger app'
+    },
+    message: "This feature is not supported on the version of Messenger you're using. Updating to the latest version will provide the best experience for sharing ABC News stories on Messenger."
+  });
 });
 
 module.exports = router;
