3ce22caab7475832744f660688444f0fe318d111
Ash Kyd <ash@kyd.com.au>
2017-07-10 10:14:40 +1000

    +unit tests

--- test/groupshare.js
+++ test/groupshare.js
@@ -1,21 +1,28 @@
 require('dotenv').config({path: 'credentials.env'});
+const fs = require('fs');
+const path = require('path');
 const supertest = require('supertest');
 const assert = require('assert');
+const nock = require('nock');
 const setup = require('./helpers/setup');
+const topStories = require('./data/topstories');
+const bingSearch = require('./data/bingsearch');
+const article = require('./data/article');
 let request;
 
 function assertStory($){
   return function(){
     const href = $(this).find('a').attr('href');
-    const shortTeaserTitle = $(this).find('a').attr('href');
-    const shortTeaserText = $(this).find('a').attr('href');
-    const imgSrc = $(this).find('img').attr('src');
+    const shortTeaserTitle = $(this).find('a').data('short-teaser-title');
+    const shortTeaserText = $(this).find('a').data('short-teaser-text');
+    const imgSrcProxied = $(this).find('img').attr('src');
+    const rimgSrc = $(this).find('a').data('image');
     const title = $(this).text().trim();
-
     assert(href.match(/http:\/\/www.abc.net.au\/news\/.*\/\d+/), 'should look like a proper URL');
     assert(!!shortTeaserTitle, 'should have a short teaser title');
     assert(!!shortTeaserText, 'should have a short teaser text');
-    assert(!!imgSrc, 'should have an image src');
+    assert(!!imgSrcProxied, 'should have a proxied image src');
+    assert(!!rimgSrc, 'should have a rimage');
     assert(!!title, 'should have a title');
   }
 }
@@ -31,6 +38,10 @@ describe('groupshare', function(){
   });
 
   it('should get index', (done) => {
+    nock('https://content-gateway.abc-prod.net.au')
+      .get('/api/v2/content/id/8418312')
+      .reply(200, topStories);
+
     request
       .get('/groupshare')
       .expect(200)
@@ -48,11 +59,27 @@ describe('groupshare', function(){
   });
 
   it('should perform a search', (done) => {
+    // get the top stories index
+    nock('https://content-gateway.abc-prod.net.au')
+      .get(/.*/)
+      .reply(200, topStories);
+
+    // get the Bing search
+    nock('https://api.cognitive.microsoft.com')
+      .get(/.*/)
+      .delay(10)
+      .reply(200, bingSearch);
+
+    // bing search requests each story individually
+    nock('https://content-gateway.abc-prod.net.au')
+      .get(/.*/)
+      .times(100)
+      .reply(200, article);
+
     request
       .get('/groupshare?search=battery')
       .expect(200)
       .expect('Content-Type', /text\/html/)
-      .catch(done)
       .then(response => {
         const cheerio = require('cheerio')
         const $ = cheerio.load(response.text);
